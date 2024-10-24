e0703f623b34ae20f822ab5422bbcbfa3d578a79
Aryansh Abhilash <144447483+AryanshAbhilash@users.noreply.github.com>
2024-10-24 16:17:45 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// JavaScript can be used for more advanced animations or interactions
+document.addEventListener('DOMContentLoaded', () => {
+    // Additional scripts can be added here if needed
+});
